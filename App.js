17d68c4c72d7d0f30b2e9e76edf810dd149713b4
Ana Clara Oliveira Leal <90358925+heyanaleal@users.noreply.github.com>
2023-03-05 01:08:32 -0300

    inicio desenvolvimento do jogo monster-attack com vue js

---
+++ App.js
@@ -0,0 +1,47 @@
+new Vue({
+    el:'#app',
+    data:{
+        running: false,
+        playerLife: 20,
+        monsterLife:100,
+    },
+    computed:{
+        hasResult(){
+            return this.playerLife == 0 || this.monsterLife == 0
+
+        }
+
+    },
+    methods: {
+        startGame(){
+            this.running = true
+            this.playerLife = 100;
+            this.monsterLife = 100;
+        },
+        attack(especial){
+            // console.log(especial, this.getRandom(5,10))
+            this.hurt(7,12, false)
+
+        },
+        hurt(min, max, especial){
+            const plus = especial ? 5 : 0
+            const hurt =this.getRandom(min + plus, max + plus)
+            this.playerLife = Math.max(this.playerLife - hurt, 0)
+
+        },
+        getRandom(min,max){
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+
+        }
+
+        
+    },
+    watch:{
+        hasResult(value){
+            if (value) this.running == false
+
+        }
+
+    }
+})
